4db46a4b71403b4c556b195246be20c9887c3c54
ajayvarma007789 <vaajay322@gmail.com>
2025-02-11 23:59:21 +0530

    fixed animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+        .animate-fadeIn {
+            animation: fadeIn 2s ease-in-out forwards;
+            animation-delay: 0.10s;
+        }
+</style>
+
+
 <script lang=ts>
     import Textanimation from "../components/textanimation.svelte";    
 </script>
@@ -11,7 +28,7 @@
     <img class="text-white sm:-mt-36 px-10 sm:w-[800px] animate-fadeIn  w-[400px]" src="/Group 14.svg" alt="i am a developer">
 </section>
 <section id="Home" class=" flex justify-center items-center h-[50vh] sm:h-screen w-full">
-    <img class="text-white -mt-36 px-10 sm:w-[900px] animate-fadeIn w-[400px]" src="/Group 1.png" alt="i am a designer">
+    <img class="text-white -mt-36 px-10 sm:w-[900px] w-[400px]" src="/Group 1.png" alt="i am a designer">
 </section>
 <section id="About" class="h-screen w-full">
     <div class="md:text-5xl sm:text-4xl text-xl font-bold font-Montserrat flex flex-col justify-center items-center">
@@ -51,17 +68,3 @@
 
 </section>
 
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-        @keyframes fadeIn {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-        .animate-fadeIn {
-            animation: fadeIn 2s ease-in-out forwards;
-        }
-</style>
