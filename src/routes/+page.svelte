fa2c6f0867aa9a18146b8950f944c63088e7438e
ajayvarma007789 <vaajay322@gmail.com>
2025-02-12 00:06:52 +0530

    fixed animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
         }
         .animate-fadeIn {
             animation: fadeIn 2s ease-in-out forwards;
-            animation-delay: 0.25s;
+            animation-delay: 0.5s;
         }
 </style>
 
