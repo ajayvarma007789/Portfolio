74c296d9aaaa1066d80ae2c4ea0e4ecad9823d4e
ajayvarma007789 <vaajay322@gmail.com>
2025-02-22 01:45:19 +0530

    added projects section

---
+++ src/components/projects.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { gsap } from "gsap";
+    import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let carouselTop: HTMLDivElement;
+    let text: HTMLDivElement;
+    let carouselBottom: HTMLDivElement;
+    let container: HTMLDivElement;
+
+    onMount(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: container,
+                start: "top center",
+                end: "bottom center",
+                scrub: 1,
+                invalidateOnRefresh: true,
+                anticipatePin: 1,
+            },
+        });
+
+        tl.to(carouselTop, { x: -900, ease: "none" }, 0)
+            .to(text, { x: -200, ease: "none" }, 0)
+            .to(carouselBottom, { x: 900, ease: "none" }, 0);
+
+        return () => {
+            // Clean up ScrollTrigger on component unmount
+            ScrollTrigger.getAll().forEach((st) => st.kill());
+        };
+    });
+</script>
+
+<div
+    class="relative flex flex-col py-6 w-full overflow-hidden"
+    bind:this={container}
+>
+    <div class="flex flex-row gap-4 md:gap-8 p-6 mt-auto md:mt-0" bind:this={carouselTop}>
+        <img src="/satwa.webp" alt="satwa" class="h-72 rounded-sm" />
+        <img src="/nasa.webp" alt="nasa space apps" class="h-72 rounded-sm" />
+        <img
+            src="/sih.webp"
+            alt="smart india hackathon"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight2nd.webp"
+            alt="Twilight Hackathon 2nd place"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight4th.webp"
+            alt="Twilight Hackathon 4th place"
+            class="h-72 rounded-sm"
+        />
+        <img src="/satwa.webp" alt="satwa" class="h-72 rounded-sm" />
+        <img src="/nasa.webp" alt="nasa space apps" class="h-72 rounded-sm" />
+        <img
+            src="/sih.webp"
+            alt="smart india hackathon"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight2nd.webp"
+            alt="Twilight Hackathon 2nd place"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight4th.webp"
+            alt="Twilight Hackathon 4th place"
+            class="h-72 rounded-sm"
+        />
+        <img src="/satwa.webp" alt="satwa" class="h-72 rounded-sm" />
+    </div>
+    <div class="font-bold text-3xl md:text-5xl py-6 border-y border-neutral-500">
+        <div bind:this={text} class="flex flex-row gap-20 font-[Montserrat] items-center">
+            <h1>ACHIEVEMENTS</h1>
+            <h1>ACHIEVEMENTS</h1>
+            <h1>ACHIEVEMENTS</h1>
+            <h1>ACHIEVEMENTS</h1>
+            <h1>ACHIEVEMENTS</h1>
+        </div>
+    </div>
+    <div
+        class="flex flex-row-reverse gap-4 md:gap-8 p-6 mb-auto md:mb-0"
+        bind:this={carouselBottom}
+    >
+        <img src="/projects/1.webp" alt="satwa" class="h-72 rounded-sm" />
+        <img src="/projects/2.webp" alt="nasa space apps" class="h-72 rounded-sm" />
+        <img
+            src="/projects/3.webp"
+            alt="smart india hackathon"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/projects/ieee ras poster 1.webp"
+            alt="Twilight Hackathon 2nd place"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight4th.webp"
+            alt="Twilight Hackathon 4th place"
+            class="h-72 rounded-sm"
+        />
+        <img src="/satwa.webp" alt="satwa" class="h-72 rounded-sm" />
+        <img src="/nasa.webp" alt="nasa space apps" class="h-72 rounded-sm" />
+        <img
+            src="/sih.webp"
+            alt="smart india hackathon"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight2nd.webp"
+            alt="Twilight Hackathon 2nd place"
+            class="h-72 rounded-sm"
+        />
+        <img
+            src="/twilight4th.webp"
+            alt="Twilight Hackathon 4th place"
+            class="h-72 rounded-sm"
+        />
+        <img src="/satwa.webp" alt="satwa" class="h-72 rounded-sm" />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,7 @@
 
 <script lang=ts>
     import Textanimation from "../components/textanimation.svelte";    
+
 </script>
 
 <header class=" mt-8 text-white font-Montserrat flex justify-center items-center md:gap-[500px] gap-[300px]">
@@ -23,13 +24,14 @@
     <p class="text-2xl  hidden sm:block">Ajay Varma</p>
 </header>
 
+
 <section id="Home" class="flex justify-center items-center sm:h-screen h-[50vh] w-full">
     <img class="text-white sm:-mt-36 px-10 sm:w-[800px] animate-fadeIn  w-[400px]" src="/Group 14.svg" alt="i am a developer">
 </section>
 <section id="Home" class=" flex justify-center items-center h-[50vh] sm:h-screen w-full">
     <img class="text-white -mt-36 px-10 sm:w-[900px] w-[400px] animate-fadeIn" src="/Group 1.png" alt="i am a designer">
 </section>
-<section id="About" class="h-screen w-full">
+<section id="About" class=" mb-16 w-full">
     <div class="lg:text-5xl md:text-4xl text-2xl font-bold font-Montserrat flex flex-col justify-center items-center">
         <Textanimation text="FRONTEND DEVELOPER"/>
         <Textanimation text="AND"/>
@@ -45,11 +47,11 @@
         </em>
     </div>
 
-    <div class="lg:text-lg md:text-md text-sm font-Montserrat  flex flex-col items-center -mb-8 opacity-60">
+    <div class="lg:text-lg md:text-md text-sm font-Montserrat  flex flex-col items-center opacity-60">
         <Textanimation text="Tools,Softwares and Libraries used"/>
     </div>
 
-    <div class="grid justify-items-center my-20 mx-10 sm:mx-40 gap-1 grid-cols-2 lg:grid-cols-8 md:grid-cols-4 overflow-scroll" >
+    <div class="grid justify-items-center mt-20 mx-10 sm:mx-40 gap-1 grid-cols-2 lg:grid-cols-8 md:grid-cols-4 overflow-visible" >
         <div title="vscode" class="flex group items-center  hover:bg-white/5 hover:backdrop-blur-lg w-full h-24 justify-center rounded-md border border-transparent hover:border-gray-700 transition-all duration-300">
             <img src="/vscode.svg" loading="lazy" alt="VS Code" class="group-hover:w-12 w-10 opacity-90 transition-all duration-300">
         </div>
@@ -110,45 +112,131 @@
         <div title="Powershell" class="flex group items-center hover:bg-white/5 hover:backdrop-blur-lg w-full h-24 justify-center rounded-md border border-transparent hover:border-gray-700 transition-all duration-300">
             <img src="/powershell.svg" loading="lazy" alt="Powershell" class="group-hover:w-12 w-10 opacity-90 transition-all duration-300">
         </div>
+        
     </div>
     
-    <div id="contact" class="grid gap-10 mb-10 mx-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center">
-        <a href="https://github.com/ajayvarma007789">
-            <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
-                <img src="/github.svg" alt="github" class="w-16">
-                <div class="">
-                    <p class="font-Montserrat lg:text-lg sm:text-md">Github</p>
-                    <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">look here for more of my projects</p></em>
-                </div>
-            </div>
-        </a>
-        <a href="https://www.instagram.com/ajaydesigns2023?igsh=MWdrZ2pwNm4yMzl5dQ==">
-            <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
-                <img src="/instagram.svg" alt="instagram" class="w-14">
-                <div >
-                    <p class="font-Montserrat lg:text-lg sm:text-md">Instagram</p>
-                    <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me through instagram</p></em>
-                </div>
-            </div>
-        </a><a href="https://www.linkedin.com/in/ajay-varma-aaa71933b/">
-            <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
-                <img src="/linkedin.svg" alt="linkedin" class="w-14">
-                <div >
-                    <p class="font-Montserrat lg:text-lg sm:text-md">Linkedin</p>
-                    <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me though linkedin</p></em>
-                </div>
-            </div>
-        </a>
-        <a href="https://x.com/AJAYVARMA123629">
-            <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
-                <img src="/twitter.svg" alt="x" class="w-14">
-                <div >
-                    <p class="font-Montserrat lg:text-lg sm:text-md">Twitter</p>
-                    <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me through twitter</p></em>
-                </div>
-            </div>
+    
+        
+    
+</section>
+
+<section id="Projects" class="w-full sm:mb-16 text-white">
+
+    <div class="text-2xl md:text-3xl lg:text-5xl font-Montserrat font-bold flex flex-col items-center mb-32">
+        <em>
+            <Textanimation text="Projects"/>
+        </em>
+    </div>
+
+    <div class="grid justify-items-center my-20 mx-10 sm:mx-40 gap-10 grid-cols-1 lg:grid-cols-4 md:grid-cols-2">
+        <div title="project" class="flex items-center justify-center ">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/3.webp" alt="project">
+        </div>
+        
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/4.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300 " src="/projects/5.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/6.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px] hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/7.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px] hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/8.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/9.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/10.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/11.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/12.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/13.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/14.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center rounded-xl">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/15.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/16.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center ">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/17.webp" alt="project">
+        </div>
+        <div title="project" class="flex items-center justify-center">
+            <img class="w-[300px]  hover:w-[350px] rounded-xl border border-transparent hover:border-gray-700 transition-all duration-300" src="/projects/2.webp" alt="project">
+        </div>
+
+    </div>
+
+   
+</section>
+
+<section id="contact">
+    <div class="flex gap-5 -mt-12 mb-10 justify-center items-center  ">
+        <a href="mailto:ajaydesigns75@gmail.com">
+        <div class="flex hover:bg-white/5 hover:backdrop-blur-lg justify-center items-center p-5 text-white rounded-md border border-transparent hover:border-gray-700 transition-all duration-300">
+            <img src="email-30.png" alt="Email"> 
+        </div>
         </a>
         
+        <div class="flex gap-5 hover:bg-white/5 hover:backdrop-blur-lg justify-center items-center p-5 text-white rounded-md border border-transparent hover:border-gray-700 transition-all duration-300">
+            <img class="w-[30px]" src="phone.png" alt="Phone"> 
+            <p>
+                +919530204516
+            </p>
+        </div>
+    </div>
+
+<div id="contact" class="grid gap-10 mb-10 mx-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center">
+    <a href="https://github.com/ajayvarma007789">
+        <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
+            <img src="/github.svg" alt="github" class="w-16">
+            <div class="">
+                <p class="font-Montserrat lg:text-lg sm:text-md">Github</p>
+                <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">look here for more of my projects</p></em>
+            </div>
+        </div>
+    </a>
+    <a href="https://www.instagram.com/ajaydesigns2023?igsh=MWdrZ2pwNm4yMzl5dQ==">
+        <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
+            <img src="/instagram.svg" alt="instagram" class="w-14">
+            <div >
+                <p class="font-Montserrat lg:text-lg sm:text-md">Instagram</p>
+                <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me through instagram</p></em>
+            </div>
+        </div>
+    </a><a href="https://www.linkedin.com/in/ajay-varma-aaa71933b/">
+        <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
+            <img src="/linkedin.svg" alt="linkedin" class="w-14">
+            <div >
+                <p class="font-Montserrat lg:text-lg sm:text-md">Linkedin</p>
+                <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me though linkedin</p></em>
+            </div>
+        </div>
+    </a>
+    <a href="https://x.com/AJAYVARMA123629">
+        <div id="github" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
+            <img src="/twitter.svg" alt="x" class="w-14">
+            <div >
+                <p class="font-Montserrat lg:text-lg sm:text-md">Twitter</p>
+                <em><p class="font-Montserrat lg:text-md sm:text-sm opacity-70">contact me through twitter</p></em>
+            </div>
+        </div>
+    </a>
+    
     <a href="https://dribbble.com/AjayVarmadesignare">
         <div id="dribbble" class="flex text-white p-5 gap-10 items-center hover:bg-white/10 hover:backdrop-blur-lg justify-center rounded-xl border  border-gray-700 transition-all duration-300">
             <img src="/dribbble.svg" alt="dribbble" class="w-14">
@@ -158,9 +246,8 @@
             </div>
         </div>
     </a>
-    
-    </div>
-    
+</div>
+
 </section>
 
 
